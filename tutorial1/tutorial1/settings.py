c4013a1b1f2115065db1ad66f8c2ab57487695a3
Ramesh Radhakrishnan <rameshvalan@gmail.com>
2021-11-03 13:32:58 +0530

    allowed hosts

--- tutorial1/tutorial1/settings.py
+++ tutorial1/tutorial1/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
-
+ALLOWED_HOSTS = ['https://rrameshapi.herokuapp.com/','localhost','127.0.0.1']
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
